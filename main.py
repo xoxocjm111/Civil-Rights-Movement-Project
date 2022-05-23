3b897625ace051254a07ac4d9322a9344b75f3fc
xoxocjm111 <57423806+xoxocjm111@users.noreply.github.com>
2022-05-23 14:56:20 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from termcolor import colored
 import os
 import colorama
 import webbrowser
+import signal
 
 colorama.init()
 
@@ -25,8 +26,28 @@ print(colored("""  _______ _             _____ _       _ _   _____  _       _   
 print('')
 print(colored('[1] Information', 'blue'))
 print('')
-user_input = print(input(colored('[] Type Here: ', 'blue')))
+print(colored('[2] Rosa Parks and the Bus Boycotts', 'red'))
+print('')
+while True:
 
-if user_input == '1':
-   #webbrowser.get(chrome_path).open(url)
-   print('hello')
+   user_input = int(input(colored('[] Type Here: ', 'blue')))
+
+   if user_input == 1:
+      webbrowser.get(chrome_path).open(url)
+      os.system('cls')
+      os.system('python main.py')
+      
+   elif user_input == 2:
+      os.system('cls')
+      print(colored("""The Montgomery Bus Boycott was a civil rights protest in which African-Americans refused to ride city buses in Montgomery, Alabama to protest segregation of seats.
+               The boycott took place from December 5, 1955 to December 20, 1956 and is considered the first large-scale American protest against apartheid. 
+               Four days before the boycott began, Rosa Parks, an African-American woman, was arrested and fined for refusing to give up her bus seat to a white man. 
+               The U.S. Supreme Court eventually ordered Montgomery to integrate its bus system, and one of the boycott's leaders named Martin Luther King Jr, 
+               became a preacher. Prominent leader of the American civil rights movement.""", 'blue'))
+      u_i = colored(input('[Y, N] Continue?: ')).lower()
+      if u_i == 'Y':
+         os.system('cls')
+         os.system('python main.py')
+      else:
+         os.system('cls')
+         exit()
